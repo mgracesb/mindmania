2666564277990466fd322f28ef6bf105f37647e7
Mercy Shyu <mgrace.shyu@gmail.com>
2024-09-18 15:51:18 +0200

    Added vite.config base url

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  base: './',
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
